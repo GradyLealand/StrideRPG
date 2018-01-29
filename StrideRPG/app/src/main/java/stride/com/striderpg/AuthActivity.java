bfc735f2b01fb03295b28401029e3641be2bdda3
Brett Currie <W0299175@campus.nscc.ca>
2018-01-29 18:07:17 -0400

    update gradle build to include the google services auth api. add simple widgets to auth layout for authenticating a user before moving into a main activity.

--- StrideRPG/app/src/main/java/stride/com/striderpg/AuthActivity.java
+++ StrideRPG/app/src/main/java/stride/com/striderpg/AuthActivity.java
@@ -2,12 +2,31 @@ package stride.com.striderpg;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class AuthActivity extends AppCompatActivity {
+    // Logging tag.
+    private static final String TAG = "AuthActivity";
+    // Result int code.
+    private static final int RC_SIGN_IN = 9001;
+
+    private FirebaseAuth mAuth;
+    private GoogleSignInClient mGoogleSignInClient;
+
+    private ImageView logoImageView;
+    private ProgressBar authProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
+
+        // Widgets
+        logoImageView = findViewById(R.id.logoImageView);
+        authProgressBar = findViewById(R.id.authProgressBar);
     }
 }
