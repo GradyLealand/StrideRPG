2ac2d8b10be838bfe12221360323d68afc8b5bc8
Brett Currie <W0299175@campus.nscc.ca>
2018-01-29 14:35:17 -0400

    Added logging for SplashActivity. Fixed issue with splash_background.xml crashing application because of the wrong drawable being chosen.

--- StrideRPG/app/src/main/java/stride/com/striderpg/SplashActivity.java
+++ StrideRPG/app/src/main/java/stride/com/striderpg/SplashActivity.java
@@ -3,8 +3,10 @@ package stride.com.striderpg;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class SplashActivity extends AppCompatActivity {
+    private static final String TAG = "SplashActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +16,7 @@ public class SplashActivity extends AppCompatActivity {
         // file as the background of the activity as a way to present
         // a screen other then blank white space while the app is starting.
         // From this point, the app will move onto authentication the current user.
+        Log.w(TAG, "Beginning AuthActivity transition now.");
         startActivity(new Intent(SplashActivity.this, AuthActivity.class));
         finish();
     }
