13c06444f5e8aaa7568dab4eb386ab02908cae82
Brett Currie <W0299175@campus.nscc.ca>
2018-02-10 10:41:53 -0400

    created a basic PushTimer class to deal with Database pushes every x seconds asynchrously.

---
+++ StrideRPG/app/src/main/java/stride/com/striderpg/global/PushTimer.java
@@ -0,0 +1,51 @@
+package stride.com.striderpg.global;
+
+
+import android.util.Log;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import stride.com.striderpg.database.FirebaseDBUtil;
+
+/**
+ * Class meant to control the Concurrent timer that will Asynchriously update the FirebaseDatabase
+ * with any changed Data in the activePlayer Player Object.
+ */
+public class PushTimer {
+
+    /**
+     * Debugging Logger tag.
+     */
+    private static final String TAG = "PushTimer";
+
+    /**
+     * Reference to the Database Utility class for Pushing new data.
+     */
+    private FirebaseDBUtil db = new FirebaseDBUtil();
+
+    /**
+     * Timer to control the timerTask and it's fixed rate execution.
+     */
+    private Timer timer = new Timer();
+
+    /**
+     * TimerTask initialized with a run() method that executes every x seconds.
+     */
+    private TimerTask timerTask = new TimerTask() {
+        @Override
+        public void run() {
+            Log.d(TAG, "attempting to push active player to the database...");
+            db.pushActivePlayer();
+        }
+    };
+
+    /**
+     * Function to schedule the TimerTask at a fixed rate.
+     */
+    public void start() {
+        timer.scheduleAtFixedRate(timerTask, 0, 10000);
+    }
+}
+
+
