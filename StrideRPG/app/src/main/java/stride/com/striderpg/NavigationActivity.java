4acbc98006748563103fba562dfa3a5c8695a6ca
Brett Currie <W0299175@campus.nscc.ca>
2018-02-12 13:51:12 -0400

    base fragments added and being used throughout navigation.

--- StrideRPG/app/src/main/java/stride/com/striderpg/NavigationActivity.java
+++ StrideRPG/app/src/main/java/stride/com/striderpg/NavigationActivity.java
@@ -1,5 +1,6 @@
 package stride.com.striderpg;
 
+import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -7,11 +8,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import stride.com.striderpg.fragments.BestiaryFragment;
+import stride.com.striderpg.fragments.DashboardFragment;
+import stride.com.striderpg.fragments.Generator.FragmentGenerator;
+import stride.com.striderpg.fragments.InventoryFragment;
+import stride.com.striderpg.fragments.LeaderboardsFragment;
+import stride.com.striderpg.fragments.QuestsFragment;
 import stride.com.striderpg.global.Globals;
 
 public class NavigationActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private FragmentGenerator generator = new FragmentGenerator();
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -20,19 +28,24 @@ public class NavigationActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(Globals.activePlayer.getUsername());
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fragmentContainer, generator.dashboardFragment).commit();
                     return true;
                 case R.id.navigation_quests:
-                    mTextMessage.setText(R.string.navbar_quests);
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fragmentContainer, generator.questsFragment).commit();
                     return true;
                 case R.id.navigation_inventory:
-                    mTextMessage.setText(R.string.navbar_inventory);
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fragmentContainer, generator.inventoryFragment).commit();
                     return true;
                 case R.id.navigation_bestiary:
-                    mTextMessage.setText(R.string.navbar_bestiary);
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fragmentContainer, generator.bestiaryFragment).commit();
                     return true;
                 case R.id.navigation_leaderboards:
-                    mTextMessage.setText(R.string.navbar_leaderboard);
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fragmentContainer, generator.leaderboardsFragment).commit();
                     return true;
             }
             return false;
@@ -42,16 +55,19 @@ public class NavigationActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bottom_nav);
+        setContentView(R.layout.activity_navigation);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.fragmentContainer, generator.dashboardFragment).commit();
+
+        mTextMessage = findViewById(R.id.message);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
 
         // Setting default stuff to appear, same as if selected in switch case
         navigation.setSelectedItemId(R.id.navigation_dashboard);
-        mTextMessage.setText(Globals.activePlayer.getUsername());
-
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+
+
 
 }
--- StrideRPG/app/src/main/java/stride/com/striderpg/fragments/Generator/FragmentGenerator.java
+++ StrideRPG/app/src/main/java/stride/com/striderpg/fragments/Generator/FragmentGenerator.java
@@ -1,8 +1,16 @@
 package stride.com.striderpg.fragments.Generator;
 
 
+import stride.com.striderpg.fragments.BestiaryFragment;
+import stride.com.striderpg.fragments.DashboardFragment;
+import stride.com.striderpg.fragments.InventoryFragment;
+import stride.com.striderpg.fragments.LeaderboardsFragment;
+import stride.com.striderpg.fragments.QuestsFragment;
+
 public class FragmentGenerator {
-
-
-
+    public final DashboardFragment dashboardFragment = new DashboardFragment();
+    public final BestiaryFragment bestiaryFragment = new BestiaryFragment();
+    public final InventoryFragment inventoryFragment = new InventoryFragment();
+    public final QuestsFragment questsFragment = new QuestsFragment();
+    public final LeaderboardsFragment leaderboardsFragment = new LeaderboardsFragment();
 }
