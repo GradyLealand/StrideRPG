5c0de4642bff9b9ccf3c9a2b4ca24ae1f9d0b633
Brett Currie <theycallmevotum@gmail.com>
2018-02-23 07:58:00 -0400

    add changes to the text generation in ActivityGenerator. Added ability to determine if an "A" or "AN" should be used when creating an Activity description. Removes ellipses from TimeParser toReadable method.

--- StrideRPG/app/src/main/java/stride/com/striderpg/rpg/generators/ActivityGenerator.java
+++ StrideRPG/app/src/main/java/stride/com/striderpg/rpg/generators/ActivityGenerator.java
@@ -24,12 +24,6 @@ public class ActivityGenerator {
     private static final String TAG = "ActivityGenerator";
 
     /**
-     * Random instance available to the ActivityGenerator class for
-     * creating new activities.
-     */
-    private static Random r = new Random();
-
-    /**
      * Generate a loot activity from the item passed.
      * @param item Item used to build activity description.
      * @return Activity.
@@ -48,40 +42,6 @@ public class ActivityGenerator {
     }
 
     /**
-     * Generate an enemy activity from the enemy passed.
-     * @param enemy Enemy used to build activity description.
-     * @return Activity.
-     */
-    public static Activity generateEnemyActivity(Enemy enemy) {
-        Activity newActivity = new Activity(
-                TimeParser.makeTimestamp(),
-                Enums.ActivityType.ENEMY,
-                generateEnemyDescription(enemy),
-                // TODO : Find real icons for enemies.
-                R.drawable.ic_troll_1300185
-        );
-
-        Log.d(TAG, String.format(G.locale, "generateEnemyActivity:success:activity=%s", newActivity));
-        return newActivity;
-    }
-
-    /**
-     * Generate a String to represent this Enemy Activities description.
-     * @param enemy Enemy.
-     * @return Activity description.
-     */
-    private static String generateEnemyDescription(Enemy enemy) {
-        String desc = String.format(G.locale,
-                "You encountered and defeated a %s\nYou earned %d experience points!",
-                enemy.getName(),
-                enemy.getExperienceReward()
-        );
-
-        Log.d(TAG, String.format(G.locale, "generateEnemyDescription:success:description=%s", desc));
-        return desc;
-    }
-
-    /**
      * Generate a String to represent this Loot Activities description.
      * @param item Item.
      * @return Activity description.
@@ -90,11 +50,10 @@ public class ActivityGenerator {
         String desc;
         if (item.isBetter(G.activePlayer.getEquipment().getItem(item.getItemType()))) {
             desc = String.format(G.locale,
-                    "You found some %s loot! %s (%s) and replaced your old equipment!\n" +
+                    "You found some %s equipment...\n%s\nYou replaced your old equipment!\n" +
                             "Power: %d\nVitality: %d\nStrength: %d\nSpeed: %d",
                     item.getItemRarity().getName(),
                     item.getName(),
-                    item.getItemType().getName(),
                     item.getPowerLevel(),
                     item.getVitalityBoost(),
                     item.getStrengthBoost(),
@@ -102,11 +61,10 @@ public class ActivityGenerator {
             );
         } else {
             desc = String.format(G.locale,
-                    "You found some %s loot! %s (%s)!\n" +
-                            "Power: %d\nVIT: %d\nSTR: %d\nSPD: %d",
+                    "You found some %s equipment...\n%s\n" +
+                            "Power: %d\nVitality: %d\nStrength: %d\nSpeed: %d",
                     item.getItemRarity().getName(),
                     item.getName(),
-                    item.getItemType().getName(),
                     item.getPowerLevel(),
                     item.getVitalityBoost(),
                     item.getStrengthBoost(),
@@ -143,8 +101,8 @@ public class ActivityGenerator {
      */
     private static String generateEnemyDefeatedDescription(Enemy enemy) {
         String desc = String.format(G.locale,
-                "You encountered and defeated a %s, earning %d experience points!",
-                enemy.getName(),
+                "You encountered and defeated %s.\nYou earned %d experience points!",
+                parseEnemyNameToProperAOrAn(enemy.getName()),
                 enemy.getExperienceReward()
         );
 
@@ -178,12 +136,24 @@ public class ActivityGenerator {
      */
     private static String generateDefeatedByEnemyDescription(Enemy enemy) {
         String desc = String.format(G.locale,
-                "You encountered and were defeated by a %s, you only earned %d experience points!",
-                enemy.getName(),
+                "You encountered and were defeated by %s.\nYou only earned %d experience points!",
+                parseEnemyNameToProperAOrAn(enemy.getName()),
                 enemy.getLevel()
         );
 
         Log.d(TAG, String.format(G.locale, "generateDefeatedByEnemyDescription:success:description=%s", desc));
         return desc;
+    }
+
+    private static String parseEnemyNameToProperAOrAn(String enemyName) {
+        String firstLetterLower = String.valueOf(enemyName.charAt(0)).toLowerCase();
+        String[] vowels = {"a", "e", "i", "o", "u"};
+
+        for (String s : vowels) {
+            if (firstLetterLower.equals(s)) {
+                return String.format(G.locale, "an %s", enemyName);
+            }
+        }
+        return String.format(G.locale, "a %s", enemyName);
     }
 }
