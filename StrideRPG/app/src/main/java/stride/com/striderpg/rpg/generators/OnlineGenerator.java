32a50c8aea53fae0e7801ce636a5fdb8cd6a0781
GradyLealand <kineco@gmail.com>
2018-03-26 13:15:55 -0300

    Online encounter chance are now based on the players speed

--- StrideRPG/app/src/main/java/stride/com/striderpg/rpg/generators/OnlineGenerator.java
+++ StrideRPG/app/src/main/java/stride/com/striderpg/rpg/generators/OnlineGenerator.java
@@ -3,6 +3,8 @@ package stride.com.striderpg.rpg.generators;
 
 import android.util.Log;
 
+import java.util.Random;
+
 import stride.com.striderpg.global.G;
 import stride.com.striderpg.rpg.Constants;
 import stride.com.striderpg.rpg.Enums;
@@ -25,29 +27,34 @@ public class OnlineGenerator {
      * with the Players current online steps count.
      */
     public static void calculateOnlineActivity() {
-        Log.d(TAG, String.format(
-                G.locale,
-                "calculateOnlineActivity:check:onlineActivitySteps=%d",
-                G.onlineActivitySteps));
+
+        Random rnd = new Random();
 
         // Check for threshold reached so an online Activity will be generated.
         if (G.onlineActivitySteps > Constants.ONLINE_ACTIVITY_STEP_THRESHOLD) {
-
+            //roll a random int between 1-100
+            int roll = rnd.nextInt(100) + 1;
+            //The Players total speed
+            int totatlSpeed = G.activePlayer.getSkills().getSpeed() + G.activePlayer.getEquipment().getItem(Enums.ItemType.BOOTS).getStatBoost();
             // Reset current sessions steps counter and generate a new
             // default Activity.
             G.onlineActivitySteps = 0;
 
-            // Generate an Activity to represent a Player Encountering an Enemy.
-            Activity activity = ActivityGenerator.generateActivityOfType(Enums.ActivityType.ENEMY);
+            //if the roll + speed modifier is grater then the encounter chance, trigger an encounter
+            if(roll + (totatlSpeed / 2) > Constants.ONLINE_ACTIVE_ENCOUNTER_CHANCE_PERCENT)
+            {
+                // Generate an Activity to represent a Player Encountering an Enemy.
+                Activity activity = ActivityGenerator.generateActivityOfType(Enums.ActivityType.ENEMY);
 
-            activity.setTimestamp(TimeParser.makeTimestamp());
+                activity.setTimestamp(TimeParser.makeTimestamp());
 
-            // Add new Activity to Players ActivityLog.
-            G.activePlayer.getActivityLog().addOnlineActivity(activity);
+                // Add new Activity to Players ActivityLog.
+                G.activePlayer.getActivityLog().addOnlineActivity(activity);
 
-            // Begin check for Player ActiveEncounter/Boss encounter.
-            if (!G.activePlayer.getActiveEncounter().isActive()) {
-                EncounterGenerator.calculateActiveEncounter(G.activePlayer);
+                // Begin check for Player ActiveEncounter/Boss encounter.
+                if (!G.activePlayer.getActiveEncounter().isActive()) {
+                    EncounterGenerator.calculateActiveEncounter(G.activePlayer);
+                }
             }
         }
     }
